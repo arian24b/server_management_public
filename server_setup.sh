80b9183ef65833ef5a925ad2dc584be3388a3e20
آریان عمرانی <a.omrani@iranserver.com>
2023-08-15 10:04:23 +0330

    fix bugs

--- server_setup.sh
+++ server_setup.sh
@@ -8,59 +8,99 @@ set -e
 
 clear
 
+GIT_REPO=https://github.com/arian24b/server_management_public
+
 # Load Template
-source <(curl -SskL https://github.com/arian24b/server_management_public/raw/main/template.sh)
+source <(curl -SskL $GIT_REPO/raw/main/template.sh)
 
 # Change /etc/resolv.conf
-$CURL https://github.com/arian24b/server_management_public/raw/main/conf/resolv.conf -o /run/systemd/resolve/resolv.conf \
-&& unlink /etc/resolv.conf \
-&& ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+RESOLVCONF=/etc/resolv.conf
+RESOLVCONF_SYSTEMD=/run/systemd/resolve/resolv.conf
+$CURL $GIT_REPO/raw/main/conf/resolv.conf -o $RESOLVCONF_SYSTEMD \
+&& unlink $RESOLVCONF \
+&& ln -s $RESOLVCONF_SYSTEMD $RESOLVCONF
+Green_msg "$RESOLVCONF changed!"
 
 # Update /etc/sysctl.conf
-$CURL https://github.com/arian24b/server_management_public/raw/main/conf/sysctl.conf -o /etc/sysctl.conf \
-&& sysctl -p
+SYSCTLCONF=/etc/sysctl.conf
+$CURL $GIT_REPO/raw/main/conf/sysctl.conf -o $SYSCTLCONF \
+&& sysctl --ignore -p --quiet
+Green_msg "$SYSCTLCONF changed!"
 
-add-apt-repository ppa:deadsnakes/ppa -y
-apt update && apt upgrade -y && apt dist-upgrade -y && apt autoclean -y
-apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoclean -y
-apt-get -y -f -q install build-essential axel nload iotop htop sudo atop git tmux screen nano ca-certificates curl gnupg lsb-release tree socat wget moreutils dnsutils unzip perl iptables libio-socket-ssl-perl libcrypt-ssleay-perl libnet-libidn-perl libio-socket-inet6-perl libsocket6-perl brotli net-tools tuned software-properties-common socat cron vim wget sendmail ipython3 python3 python3-pip zstd zip python3-venv python3.11 python3.11-venv dphys-swapfile
+# Update and install packages
+add-apt-repository ppa:deadsnakes/ppa --yes
 
-dpkg --configure -a
+apt-get update --fix-missing --quiet --yes
+apt-get upgrade --fix-broken --fix-missing --quiet --yes
+apt-get dist-upgrade --fix-broken --fix-missing --quiet --yes
+Green_msg "System updated!"
 
+apt-get --fix-broken --fix-missing --quiet --yes install nload iotop htop atop axel curl \
+wget build-essential sudo git tmux screen nano ca-certificates gnupg lsb-release tree socat \
+moreutils dnsutils unzip perl iptables libio-socket-ssl-perl libcrypt-ssleay-perl \
+libnet-libidn-perl libio-socket-inet6-perl libsocket6-perl brotli net-tools tuned \
+software-properties-common socat cron vim wget sendmail ipython3 python3 python3-pip \
+zstd zip python3-venv python3.11 python3.11-venv dphys-swapfile libc++-dev
+Green_msg "Packages are installed!"
+
+apt-get --fix-broken --fix-missing --quiet --yes autoremove
+apt-get --fix-missing --quiet --yes clean
+apt-get --fix-missing --quiet --yes autoclean
+
+dpkg --configure
+Green_msg "apt cleaned"
+
+# Update ca-certificates
 update-ca-certificates
+Green_msg "ca-certificates Updated!"
 
-tuned-adm profile network-latency
+# Config tuned
+TUNEDPROFILE=network-latency
+tuned-adm profile $TUNEDPROFILE
 tuned-adm verify
+Green_msg "tuned profile set to $TUNEDPROFILE"
 
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
+# Install pip for python3.11
+$CURL https://github.com/pypa/get-pip/raw/main/public/get-pip.py | python3.11
 
-bash <(curl -sSL https://get.docker.com)
+# Install docker
+bash <($CURL https://get.docker.com)
 
-$CURL https://github.com/arian24b/server_management_public/raw/main/conf/dphys-swapfile -o /etc/dphys-swapfile \
+# Config swap
+$CURL $GIT_REPO/raw/main/conf/dphys-swapfile -o /etc/dphys-swapfile \
 && dphys-swapfile setup \
 && dphys-swapfile swapon
+Green_msg "swap created!"
 
-bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
+# Install marzban
+bash -c "$($CURL https://raw.githubusercontent.com/arian24b/server_management_public/main/marzban/marzban_installer.sh)" @ install
 
 # Add ssh-key to root user
 mkdir -p /root/.ssh \
 && if ! grep -m1 -qs 'Qjae3u7rMlK1oEOw' /root/.ssh/authorized_keys; then $CURL https://github.com/arian24b.keys >> /root/.ssh/authorized_keys; fi
 ssh-keygen -A
+Green_msg "ssh-key installed!"
 
 # Generate tmp file
-fallocate -l 2G /home/.tempfilefordelete
+TMPFILEDELETE=/home/.tempfilefordelete
+fallocate -l 2G $TMPFILEDELETE
+Green_msg "create tmp file in $TMPFILEDELETE"
 
 # enable rc.local
-touch /etc/rc.local
-cat << 'EOF' > /etc/rc.local
+RCLOCAL=/etc/rc.local
+touch $RCLOCAL
+cat << 'EOF' > $RCLOCAL
 #!/usr/bin/env bash
 EOF
-chmod +x /etc/rc.local \
+chmod +x $RCLOCAL \
 && systemctl enable rc-local \
 && systemctl restart rc-local
+Green_msg "$RCLOCAL installd!"
 
 # Setup backup
-mkdir -p /home/arian/backup
-$CURL https://github.com/arian24b/telegrambotapi/raw/main/telegram-bot-api -o /home/arian/backup/telegram-bot-api
-$CURL https://github.com/arian24b/telegrambotapi/raw/main/start_bot.sh.example -o /home/arian/backup/start_bot.sh
-$CURL https://github.com/arian24b/server_management_public/raw/main/backup/backup_to_telegram.sh -o /home/arian/backup/backup_to_telegram.sh
+BACKUPPATH=/home/arian/backup
+mkdir -p $BACKUPPATH
+$CURL https://github.com/arian24b/telegrambotapi/raw/main/telegram-bot-api -o $BACKUPPATH/telegram-bot-api
+$CURL https://github.com/arian24b/telegrambotapi/raw/main/start_bot.sh.example -o $BACKUPPATH/start_bot.sh
+$CURL $GIT_REPO/raw/main/backup/backup_to_telegram.sh -o $BACKUPPATH/backup_to_telegram.sh
+chmod +x $BACKUPPATH/telegram-bot-api
