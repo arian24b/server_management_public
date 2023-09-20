76fa14afc882e2dacb0db003cabf10a0a1774f04
آریان عمرانی <a.omrani@iranserver.com>
2023-09-20 10:53:58 +0330

    add rtt command

--- README.md
+++ README.md
@@ -24,3 +24,17 @@ bash <(curl -sSkL https://get.docker.com)
 ```bash
 hostnamectl set-hostname server.domain.com
 ```
+
+# Config tunnel
+
+```bash
+ir servers:
+bash -c "$(curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/installer.sh)" @ ir
+
+eu servers:
+bash -c "$(curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/installer.sh)" @ ir
+
+systemctl daemon-reload \
+&& systemctl start rtt.service \
+&& systemctl enable rtt.service
+```
---
+++ rtt/installer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# Auther: ArianOmrani - https://github.com/arian24b/
+# git repository: https://github.com/arian24b/server_management_public
+
+set -e
+# set -x
+
+clear
+
+GIT_REPO=https://github.com/arian24b/server_management_public
+
+# Load Template
+source <(curl -SskL $GIT_REPO/raw/main/template.sh)
+
+
+RTT_PATH=/home/marzban/rtt
+mkdir -P $RTT_PATH
+cd $RTT_PATH
+bash <(curl -sSkL https://raw.githubusercontent.com/radkesvat/ReverseTlsTunnel/master/install.sh)
+
+if $1 == "ir"; then
+    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/ir_rtt.service -o /etc/systemd/system/rtt.service
+elif $1 == "ir"; then
+    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/eu_rtt.service -o /etc/systemd/system/rtt.service
+fi
