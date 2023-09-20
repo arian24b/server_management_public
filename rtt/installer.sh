923be6342749f3b157e6092221682ba2cf52e817
آریان عمرانی <a.omrani@iranserver.com>
2023-09-20 11:02:28 +0330

    add rtt.service

--- rtt/installer.sh
+++ rtt/installer.sh
@@ -19,8 +19,10 @@ mkdir -p $RTT_PATH
 cd $RTT_PATH
 bash <(curl -sSkL https://raw.githubusercontent.com/radkesvat/ReverseTlsTunnel/master/install.sh)
 
+curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/rtt.service -o /etc/systemd/system/rtt.service
+
 if [[ $1 == "ir" ]]; then
-    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/ir_rtt.service -o /etc/systemd/system/rtt.service
+    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/ir_start.sh -o $RTT_PATH/start.sh
 elif [[ $1 == "eu" ]]; then
-    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/eu_rtt.service -o /etc/systemd/system/rtt.service
+    curl -sSkL https://github.com/arian24b/server_management_public/raw/main/rtt/ir_start.sh -o $RTT_PATH/start.sh
 fi
