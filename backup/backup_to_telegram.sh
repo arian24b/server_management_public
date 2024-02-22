9905224294d31ca905f1f63e2dd50731863dee97
arian <a.omrani@limoo.host>
2024-02-22 11:48:34 +0330

    add comment and kill bot api pid after creating backup

--- backup/backup_to_telegram.sh
+++ backup/backup_to_telegram.sh
@@ -3,56 +3,86 @@
 # Auther: ArianOmrani - https://github.com/arian24b/
 # git repository: https://github.com/arian24b/server_management_public
 
-set -e
+# Exit immediately if a command exits with a non-zero status
+#set -e
 # set -x
 
-clear
+# Clear the terminal
+#clear
 
+# Define the git repository URL
 GIT_REPO=https://github.com/arian24b/server_management_public
 
-# Load Template
+# Load the template script
 source <(curl -SskL $GIT_REPO/raw/main/template.sh)
 
 # Declare Paths & Settings.
+# Directory path
 DIR_PATH=/home
+# Temporary backup path
 TMP_BACKUP_PATH=$(mktemp -d)
+# Telegram bot token
 TELEGRAM_TOKEN="0123456789:AAAAAAAAAAAAAAAAAAAAAAAAAA"
-TM_CHATID="409804739"
+# Telegram chat ID
+TM_CHATID="123456789"
+# Export the hostname IP
 export IP=$(hostname -I)
+# Export the hostname
 export HOSTNAME=$(hostname)
 
-# Send message
+# Send message function
+# Params:
+#   $1 - The message to send
 function send_message() {
-  curl -F chat_id="$TM_CHATID" -F text="$1" "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" >/dev/null 2>&1
+    curl -F chat_id="$TM_CHATID" -F text="$1" "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" >/dev/null 2>&1
 }
 
-# Chack TM_CHATID is number
+# Check if TM_CHATID is a number
 if [[ ! $TM_CHATID =~ ^\-?[0-9]+$ ]]; then
-  abort "${TM_CHATID} is not a number.";
+    abort "${TM_CHATID} is not a number.";
 fi
 
+# Start time
 START_TIME=$DATE
 
+# Run the bot API in the background
 bash /home/arian/backup/run_bot_api.sh &
+BOTAPIPID=$!
 
+# Send a message that the backup process has started
 send_message "Start creating backup from $DIR_PATH $START_TIME"
 
+# Loop through each directory in the specified path
 for HOMEDIR in $(ls -1 $DIR_PATH); do
-  BACKUP_PATH="$TMP_BACKUP_PATH/$HOMEDIR-Backup-$DATE.zip"
-  PATHS="$DIR_PATH/$HOMEDIR"
-  TITEL="Backup from: $PATHS"
-  CAPTION="$TITEL
-  <code>$DATE</code>
-  <code>$IP</code>"
+    # Define the backup path
+    BACKUP_PATH="$TMP_BACKUP_PATH/$HOMEDIR-Backup-$DATE.zip"
+    # Define the path to backup
+    PATHS="$DIR_PATH/$HOMEDIR"
+    # Define the title
+    TITEL="Backup from: $PATHS"
+    # Define the caption
+    CAPTION="$TITEL
+    <code>$DATE</code>
+    <code>$IP</code>"
 
-  zip -r -q -T -y -9 $BACKUP_PATH $PATHS >/dev/null 2>&1
+    # Create a zip archive of the specified path
+    zip -r -q -T -y -1 $BACKUP_PATH $PATHS >/dev/null 2>&1
 
-  curl -F chat_id="$TM_CHATID" -F caption="$CAPTION" -F parse_mode="HTML" -F document=@"$BACKUP_PATH" "http://127.0.0.1:5687/bot${TELEGRAM_TOKEN}/sendDocument" >/dev/null 2>&1 # https://api.telegram.org/bot
+    # Send the backup file to Telegram
+    curl -F chat_id="$TM_CHATID" -F caption="$CAPTION" -F parse_mode="HTML" -F document=@"$BACKUP_PATH" "http://127.0.0.1:5687/bot${TELEGRAM_TOKEN}/sendDocument" >/dev/null 2>&1 # https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument
 
+    # Remove the backup file
+    rm -f $BACKUP_PATH
 done
 
+# Kill the bot API process
+kill -9 $BOTAPIPID
+
+# Remove the temporary backup path
 rm -rf $TMP_BACKUP_PATH
 
+# End time
 END_TIME=$DATE
 
+# Send a message that the backup process has completed
 send_message "Backup from server $HOSTNAME: $IP from path: $DIR_PATH in $START_TIME to $END_TIME."
