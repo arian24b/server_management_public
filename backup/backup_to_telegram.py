dd38aea092fc89f2bf8136b4191b63ceb30704ee
arian <arian24b@gmail.com>
2024-08-20 23:52:29 +0330

    some good changes and improvement

--- backup/backup_to_telegram.py
+++ backup/backup_to_telegram.py
@@ -8,20 +8,24 @@ from os import listdir, path, remove, walk
 from tempfile import mkdtemp
 from datetime import datetime
 from shutil import rmtree
-from subprocess import Popen, CalledProcessError, DEVNULL, run
+from subprocess import Popen, DEVNULL, run
 from typing import Tuple
 from zipfile import ZipFile
+from time import sleep
 
 
 # Declare Paths & Settings
 HOSTNAME = gethostname()
+IP=request(method="get", url="https://api.ipify.org").text if gethostbyname(HOSTNAME) in "127.0.0.1" else gethostbyname(HOSTNAME)
 DIR_PATH = "/home"
-TELEGRAM_TOKEN = ""
-CHAT_ID = ""
-API_ID = ""
-API_HASH = ""
-API_PORT = "5687"
-API_URL = f"http://127.0.0.1:{API_PORT}"
+TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_CHAT_ID = ""
+TELEGRAM_API_ID = ""
+TELEGRAM_API_HASH = ""
+TELEGRAM_LOCAL_API_PORT = "5687"
+TELEGRAM_LOCAL_API_URL = f"http://127.0.0.1:{TELEGRAM_LOCAL_API_PORT}"
+TELEGRAM_BOT_API_BINARY_NAME = "telegram-bot-api"
+TELEGRAM_BOT_API_BINARY_PATH = f"/home/arian/backup/{TELEGRAM_BOT_API_BINARY_NAME}"
 
 
 def run_telegram_bot_api() -> Tuple[Popen, str]:
@@ -32,13 +36,10 @@ def run_telegram_bot_api() -> Tuple[Popen, str]:
         Tuple[Popen, str]: A tuple containing the Popen object representing the running process and the temporary directory path.
 
     Raises:
-        CalledProcessError: If an error occurs while executing the Telegram Bot API binary.
-
+        None
     """
-    # Declare Paths & Settings
-    TELEGRAM_BOT_API_BINARY_NAME = "telegram-bot-api"
-    TELEGRAM_BOT_API_BINARY_PATH = f"/home/arian/backup/{TELEGRAM_BOT_API_BINARY_NAME}"
-    TEMP_DIR = mkdtemp(prefix=f"{TELEGRAM_BOT_API_BINARY_NAME}-")  # Create a temporary directory
+    # Create a temporary directory
+    TEMP_DIR = mkdtemp(prefix=f"{TELEGRAM_BOT_API_BINARY_NAME}-")
 
     # Kill all instances of the telegram-bot-api binary
     kill_args = [
@@ -51,51 +52,33 @@ def run_telegram_bot_api() -> Tuple[Popen, str]:
     # Arguments to pass to the telegram-bot-api binary
     run_args = [
         TELEGRAM_BOT_API_BINARY_PATH,
-        f'--api-id={API_ID}',
-        f'--api-hash={API_HASH}',
-        f'--http-port={API_PORT}',
-        f'--dir={TEMP_DIR}',
-        f'--temp-dir={TEMP_DIR}',
-        '--local'
+        f"--api-id={TELEGRAM_API_ID}",
+        f"--api-hash={TELEGRAM_API_HASH}",
+        f"--http-port={TELEGRAM_LOCAL_API_PORT}",
+        f"--dir={TEMP_DIR}",
+        f"--temp-dir={TEMP_DIR}",
+        "--local"
     ]
 
     # Execute the telegram-bot-api binary with the specified arguments
     try:
         bot_api_process = Popen(args=run_args, stdout=DEVNULL, stderr=DEVNULL)
-    except CalledProcessError as e:
+        sleep(0.5)
+    except Exception as e:
         # print(f"An error occurred: {e}")
         rmtree(TEMP_DIR)  # Clean up if error occurs
         raise
+
     return bot_api_process, TEMP_DIR
 
 
-def domain_to_ip(domain: str = HOSTNAME) -> str:
-    """
-    Returns the IP address associated with the given domain.
-
-    Args:
-        domain (str): The domain to resolve to an IP address. Defaults to HOSTNAME.
-
-    Returns:
-        str: The IP address associated with the given domain.
-
-    This function first attempts to resolve the domain to an IP address using the gethostbyname function.
-    If the resolved IP address is "127.0.0.1", it makes a GET request to https://api.ipify.org to obtain the public IP address.
-    The obtained IP address is then returned.
-    """
-    ip = gethostbyname(domain)
-    if ip == "127.0.0.1":
-        ip = request(method="get", url="https://api.ipify.org").text
-    return ip
-
-
-def telegram_send_message(message: str, chat_id: str = CHAT_ID) -> Response:
+def telegram_send_message(message: str, chat_id: str = TELEGRAM_CHAT_ID) -> Response:
     """
     Sends a message to a Telegram chat using the Telegram Bot API.
 
     Args:
         message (str): The message to send.
-        chat_id (str, optional): The ID of the chat to send the message to. Defaults to CHAT_ID.
+        chat_id (str, optional): The ID of the chat to send the message to. Defaults to TELEGRAM_CHAT_ID.
 
     Returns:
         Response: The response object from the API call.
@@ -104,29 +87,31 @@ def telegram_send_message(message: str, chat_id: str = CHAT_ID) -> Response:
     The message is sent to the specified chat using the provided token and chat ID.
     The function returns the response object from the API call.
     """
-    url = f"{API_URL}/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {'chat_id': chat_id, 'text': message}
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message}
     response = request(method="POST", url=url, data=payload)
+
     return response
 
 
-def telegram_send_file(file_path: str, caption: str, chat_id: str = CHAT_ID) -> Response:
+def telegram_send_file(file_path: str, caption: str, chat_id: str = TELEGRAM_CHAT_ID) -> Response:
     """
     Sends a file to a Telegram chat using the specified file path, caption, and chat ID.
 
     Args:
         file_path (str): The path to the file to be sent.
         caption (str): The caption to be included with the file.
-        chat_id (str, optional): The ID of the chat to send the file to. Defaults to CHAT_ID.
+        chat_id (str, optional): The ID of the chat to send the file to. Defaults to TELEGRAM_CHAT_ID.
 
     Returns:
         Response: The response object containing the result of the file sending request.
     """
-    with open(file_path, 'rb') as file:
-        url = f"{API_URL}/bot{TELEGRAM_TOKEN}/sendDocument"
-        files = {'document': file}
-        payload = {'chat_id': chat_id, 'caption': caption, 'parse_mode': 'HTML'}
+    with open(file_path, "rb") as file:
+        url = f"{TELEGRAM_LOCAL_API_URL}/bot{TELEGRAM_BOT_TOKEN}/sendDocument"
+        files = {"document": file}
+        payload = {"chat_id": chat_id, "caption": caption, "parse_mode": "HTML"}
         response = request(method="POST", url=url, files=files, data=payload)
+
     return response
 
 
@@ -147,6 +132,7 @@ def retrieve_file_paths(dir_path: str) -> list:
         for filename in files:
             # Create the full filepath by using os module.
             filePaths.append(path.join(root, filename))
+
     return filePaths
 
 
@@ -170,19 +156,18 @@ def create_backup(path_to_backup: str, backup_path: str) -> None:
         - The function uses the ZipFile class from the zipfile module to create the zip file.
         - Each file in the filePaths list is written to the zip file using the write method of the ZipFile class.
     """
-    if not backup_path.endswith(".zip"):
-        backup_path += ".zip"
+    backup_path=backup_path if backup_path.endswith(".zip") else backup_path + ".zip"
 
     # Call the function to retrieve all files and folders of the assigned directory
     filePaths = retrieve_file_paths(path_to_backup)
 
     # print the list of files to be zipped
-    # print('The following list of files will be zipped:')
+    # print("The following list of files will be zipped:")
     # for fileName in filePaths:
     #     print(fileName)
 
     # write files and folders to a zipfile
-    with ZipFile(backup_path, 'w') as zfile:
+    with ZipFile(backup_path, "w") as zfile:
         # write each file seperately
         for file in filePaths:
             zfile.write(file)
@@ -193,16 +178,14 @@ def main() -> None:
     Runs the main backup process, which includes starting the Telegram Bot API in the background, getting the IP address, sending a message that the backup process has started, creating a temporary backup path, looping through each directory in the specified path, defining the backup path, defining the title and caption, creating a zip archive of the specified path, sending the backup file to Telegram, removing the backup file, killing the bot API process, removing the temporary backup path, and sending a message that the backup process has completed.
     """
     # Start time
-    start_time = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    start_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+    # Send a message that the backup process has started
+    telegram_send_message(message=f"Start creating backup from {DIR_PATH} {start_time}")
 
     # Run the bot API in the background
     bot_api_process, temp_dir = run_telegram_bot_api()
 
-    # Get the IP address
-    IP = domain_to_ip()
-
-    # Send a message that the backup process has started
-    telegram_send_message(message=f"Start creating backup from {DIR_PATH} {start_time}")
 
     # Create a temporary backup path
     tmp_backup_path = mkdtemp(prefix="backup-")
@@ -227,10 +210,8 @@ def main() -> None:
             remove(backup_path)
 
     # End time
-    end_time = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    end_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
-    # Send a message that the backup process has completed
-    telegram_send_message(message=f"Backup from server {HOSTNAME}: {IP} from path: {DIR_PATH} in {start_time} to {end_time}.")
 
     # Kill the bot API process
     bot_api_process.kill()
@@ -238,6 +219,9 @@ def main() -> None:
     # Remove the temporary backup path
     rmtree(tmp_backup_path)
     rmtree(temp_dir)
+
+    # Send a message that the backup process has completed
+    telegram_send_message(message=f"Backup from server {HOSTNAME}: {IP} from path: {DIR_PATH} in {start_time} to {end_time}.")
 
 
 if __name__ == "__main__":
